137c7b9908f12e16a4dc6c0c3a791b418073b9dc
James Garcia <james.garcia1201@outlook.com>
2024-08-19 23:48:05 -0500

    adding back default page, changing home text

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -4,16 +4,14 @@ const HomePage = () => {
     return (
         <HomeContainer>
             <Bio>
-                <span>Hi, I’m James Garcia,</span>
+                <span>Hi, I’m James,</span>
                 <span>a Fullstack Engineer</span>
                 <span>from Houston, Texas.</span>
                 <span>I specialize in</span>
-                <span>JavaScript, Scala,</span>
-                <span>Python, and Java</span>
-                <span>to build scalable</span>
-                <span>and efficient applications.</span>
-                <span>Passionate about technology</span>
-                <span>and problem-solving,</span>
+                <span>building scalable</span>
+                <span>applications paired</span>
+                <span>with intuitive</span>
+                <span>user interfaces.</span>
                 <span>I’m always looking</span>
                 <span>for new challenges.</span>
             </Bio>
