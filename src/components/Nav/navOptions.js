100b6bf61abea82cf5b6e311f0e074daded41760
James Garcia <james.garcia1201@outlook.com>
2025-04-03 10:15:36 -0500

    removing projects

--- src/components/Nav/navOptions.js
+++ src/components/Nav/navOptions.js
@@ -2,6 +2,5 @@
 export const navOptions = [
     { val: 'home', label: 'Home' },
     { val: 'resume', label: 'Resume'},
-    { val: 'projects', label: 'Projects '},
     { val: 'contact', label: 'Contact' },
 ]
